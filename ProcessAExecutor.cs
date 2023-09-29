4b22f61bd491ee28e4122ed1cfd20e9a2087f9f2
Arielle Sathler Santos <arielle.santos@pottencial.com.br>
2023-09-29 19:02:37 -0300

    Add project files.

---
+++ ProcessAExecutor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Poc.CronJob.HostedService
+{
+    public class ProcessAExecutor : BackgroundService, IAsyncExecutor
+    {
+        public Task WhenDone => base.ExecuteTask!;
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Start A");
+
+            await Task.Delay(2000, stoppingToken);
+
+            Console.WriteLine("Finish A");
+        }
+    }
+
+    public class ProcessBExecutor : BackgroundService, IAsyncExecutor
+    {
+        public Task WhenDone => base.ExecuteTask!; 
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Console.WriteLine("Start B");
+
+            await Task.Delay(5000, stoppingToken);
+
+            Console.WriteLine("Finish  B");
+        }
+    }
+
+    public interface IAsyncExecutor
+    {
+        Task WhenDone { get; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Poc.CronJob.HostedService;
+
+var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<ProcessAExecutor>();
+                services.AddHostedService<ProcessBExecutor>();
+            })
+            .Build();
+
+
+using (host)
+{
+    await host.StartAsync();
+
+    var services = host.Services.GetServices<IHostedService>().Cast<IAsyncExecutor>();
+    
+    await Task.WhenAll(services.Select(a=> a.WhenDone));
+
+    await host.StopAsync();
+}
+
+
+
